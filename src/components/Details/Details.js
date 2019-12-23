fdeb58122fdf173811c17f042625a6eb88f22e2b
Jamie Richison <jamierichi@yahoo.com>
2019-12-22 20:05:00 -0600

    added icons/styled Details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import { List, ListItem, Button } from '@material-ui/core/';
+import BackspaceIcon from '@material-ui/icons/Backspace';
+import EditIcon from '@material-ui/icons/Edit';
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+      },
+    iconSmall: {
+    fontSize: 20,
+    },
+    leftIcon: {
+    marginRight: theme.spacing.unit,
+    },
+    listItem: {
+        textAlign: 'right',
+        justifyContent: 'flex-center'
+    },
+    list: {
+        margin: '0 auto',
+        width: '100%',
+        justifyContent: 'center',
+        maxWidth: '12rem'
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+      },
+});
+
 
 class Details extends Component{
 
@@ -12,10 +42,15 @@ class Details extends Component{
     }
     
     render(){
+        const { classes } = this.props;
         return(
             <>
-            <button onClick={this.goToHome}>Go Back</button>
-            <button onClick={(event)=> this.editDetails(event, this.props.details)}>Edit</button>
+            <Button variant="contained" color="secondary" className={classes.button} 
+                onClick={this.goToHome}> <BackspaceIcon className={classes.leftIcon} />Go Back
+            </Button>
+            <Button variant="contained" color="primary" className={classes.button} 
+                onClick={(event)=> this.editDetails(event, this.props.details)}>Edit <EditIcon className={classes.rightIcon} />
+            </Button>
             {this.props.reduxStore.details.map( (item, i) =>{ return (
                 <div key={i}>
                 <h3>{item.title}</h3> 
@@ -23,13 +58,11 @@ class Details extends Component{
                 </div>
             )}) }
             <h3>Genres</h3>
-            <ul>
+            <List className={classes.list}>
             {this.props.reduxStore.genres.map( (item, i) =>{ return (
-                <div key={i}>
-                <li>{item.name}</li> 
-                </div>
+                <ListItem className={classes.listItem}key={i}>{item.name}</ListItem> 
             )}) }
-            </ul>
+            </List>
             </>
         )
     }
@@ -39,4 +72,4 @@ const putReduxStateOnProps = (reduxStore) => ({
     reduxStore,
 });
 
-export default connect(putReduxStateOnProps)(Details);
+export default withStyles(styles)(connect(putReduxStateOnProps)(Details));
