a0b63529bcdd4083bc86ff1ff4769f4924c9af04
Jamie Richison <jamierichi@yahoo.com>
2019-12-22 18:42:28 -0600

    added functionality to display details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 class Details extends Component{
+
+    goToHome = ()=>{
+        this.props.history.push(`/`);
+    }
+
+    editDetails = ( event, id ) =>{
+        this.props.history.push('/edit')
+    }
+    
     render(){
         return(
             <>
-            <h2>Details</h2>
+            <button onClick={this.goToHome}>Go Back</button>
+            <button onClick={(event)=> this.editDetails(event, this.props.details)}>Edit</button>
+            {this.props.reduxStore.details.map( (item, i) =>{ return (
+                <div key={i}>
+                <h3>{item.title}</h3> 
+                <p>{item.description}</p>
+                </div>
+            )}) }
             </>
         )
     }
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,11 +3,20 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class MovieItem extends Component {
+    
+    goToDetails = (event, id) => {
+        this.props.dispatch({ type: 'GET_DETAILS', payload: {id: id} });
+    }
+
     render(){
         return(
             <div key={this.props.reduxStore.movies[0].id}>
                 <h3>{this.props.item.title}</h3>
-                <img src={this.props.item.poster} alt={this.props.item.title} />
+                <Link to="/details">
+                    <img 
+                    onClick={(event) => this.goToDetails(event, this.props.item.id)}
+                    src={this.props.item.poster} alt={this.props.item.title} />
+                </Link>
                 <h4>Description</h4>
                 <p>{this.props.item.description}</p>
             </div>
