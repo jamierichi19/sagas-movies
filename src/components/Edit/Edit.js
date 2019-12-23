9150d6a7c8314fb3a3015dd5070be3fc36a9c810
Jamie Richison <jamierichi@yahoo.com>
2019-12-22 19:00:37 -0600

    added functionality to edit movie details

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Edit extends Component{
+
+    state= {
+        title: '',
+        description: '',
+        id: this.props.reduxStore.details[0].id
+    }
+
+    editDetails = (event) => {
+        this.props.dispatch({ type: 'EDIT_DETAILS', payload: this.state});
+        this.props.dispatch({ type: 'GET_DETAILS', payload: {id: this.state.id} });
+        this.props.history.push('/details')
+    }
+
+    goBack = ()=>{
+        this.props.history.push(`/details`);
+    }
+
+      // keeps track of information being typed in form
+      handleChange = (event, propertyName) => {
+        console.log('Got a change:', event.target.value, propertyName );
+        this.setState({
+          [propertyName]: event.target.value
+      })
+    }
     render(){
         return(
-            <h1>Edit</h1>
+            <>
+            <input type="text" placeholder="Updtate Title" onChange={(event)=>this.handleChange(event, 'title')} value={this.state.title}/>
+            <br></br>
+            <textarea type="text" placeholder="Update Description" onChange={(event)=>this.handleChange(event, 'description')} value={this.state.description} />
+            <br></br>
+            <button onClick={this.goBack}>Cancel</button>
+            <button onClick={(event) => this.editDetails(event)} >Save</button>
+            </>
         )
     }
 }
